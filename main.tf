2227fc03fdab7ea8ae9f82b28927348a5fe030ff
vishnuvarman97 <148313919+vishnuvarman97@users.noreply.github.com>
2024-09-12 12:12:40 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,3 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-unique-bucket-name-123456" # Change to a unique bucket name
   # Add any other required configurations here
 }
-
-# Output the bucket name
-output "bucket_name" {
-  value = aws_s3_bucket.my_bucket.bucket
-}
